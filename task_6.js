f3628dc8035b9a802ed4f1b9edbba3aba0cf7632
Serjikdn <serjik.dn@gmail.com>
2023-11-11 13:12:12 +0200

    task 6 fix add blur

--- task_6.js
+++ task_6.js
@@ -2,9 +2,14 @@ const backgroundItem = document.querySelectorAll('.form fieldset');
 const item = document.querySelectorAll('.text');
 
 item.forEach((value, index) => {
+    value.addEventListener('focus', () => {
+        backgroundItem.forEach((e) => {
+            e.classList.remove('select-input');
+        });
+        backgroundItem[index].classList.add('select-input');
+    });
+    value.addEventListener('blur', () => {
+        backgroundItem[index].classList.remove('select-input');
+    });
+});
 
-    value.addEventListener('click', () => {
-        backgroundItem.forEach((e)=> e.classList.remove('select-input'))
-        backgroundItem[index].className = 'select-input';
-    })
-})
