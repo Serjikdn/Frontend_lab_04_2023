b5f6c49b0cfbc444ade55ec0933ef0378c6dd422
Serjikdn <serjik.dn@gmail.com>
2023-11-11 19:46:44 +0200

    fix task 7

--- task_7.js
+++ task_7.js
@@ -2,15 +2,41 @@ const shape = document.querySelector('.shape');
 const width = document.querySelector('#widthShape');
 const height = document.querySelector('#heightShape');
 const rotate = document.querySelector('#rotateShape');
+const widthNum = document.querySelector('#widthNumber');
+const heightNum = document.querySelector('#heightNumber');
+const rotateNum = document.querySelector('#rotateNumber');
+widthNum.value = width.value;
+heightNum.value = height.value;
+rotateNum.value = rotate.value;
 
 function setParam(){
-    console.log(width.value)
     shape.style.width = width.value +'px';
     shape.style.height = height.value +'px';
     shape.style.transform = `rotate(${rotate.value}deg)`;
 
 }
 setParam();
-width.addEventListener('input', setParam);
-height.addEventListener('input', setParam);
-rotate.addEventListener('input', setParam);
+width.addEventListener('input', () => {
+    widthNum.value = width.value;
+    setParam();
+});
+height.addEventListener('input', () => {
+    heightNum.value = height.value;
+    setParam();
+});
+rotate.addEventListener('input', () => {
+    rotateNum.value = rotate.value;
+    setParam();
+});
+widthNum.addEventListener('input', () => {
+    width.value = widthNum.value;
+    setParam();
+});
+heightNum.addEventListener('input', () => {
+    height.value = heightNum.value;
+    setParam();
+});
+rotateNum.addEventListener('input', () => {
+    rotate.value = rotateNum.value;
+    setParam();
+});
